27b8e68b68241d64ef196d22f8e7137fbc03e1be
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-02-09 19:34:07 +0530

    Time: 31 ms (33.06%), Space: 44.5 MB (95.85%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -33,14 +33,114 @@ class Solution {
         return dp[i][j1][j2] = maxi;
     }
     public int cherryPickup(int[][] grid) {
+        // int n = grid.length; 
+        // int m = grid[0].length; 
+        // int dp[][][] = new int[n][m][m];
+        // for(int[][] layer : dp){
+        //     for(int[] row : layer){
+        //         Arrays.fill(row , -1) ;
+        //     }
+        // }
+        // return func(0 , 0 , m-1 , dp , grid , m , n); 
+
+        // using tabulation :- 
+
+        // int n = grid.length; 
+        // int m = grid[0].length; 
+        // int dp[][][] = new int[n][m][m];
+        // for(int[][] layer : dp){
+        //     for(int[] row : layer){
+        //         Arrays.fill(row , -1) ;
+        //     }
+        // }
+        // for(int j1 = 0 ; j1 < m ; j1++){
+        //     for(int j2 = 0 ; j2 < m ;j2++){
+        //         if(j1 == j2){
+        //             dp[n-1][j1][j2] = grid[n-1][j1]; 
+        //         }
+        //         else{
+        //             dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2]; 
+        //         }
+        //     }
+        // }
+        // for(int i = n-2 ; i>= 0 ; i--){
+        //     for(int j1 = 0 ; j1 < m ; j1 ++){
+        //         for(int j2 = 0 ; j2 < m ; j2 ++){
+        //             int maxi = Integer.MIN_VALUE ; 
+        //             for(int dj1 = -1 ; dj1 <= 1 ; dj1 ++){
+        //                 for(int dj2 = -1 ; dj2 <= 1 ; dj2 ++){
+        //                     int value = 0 ;
+        //                     if(j1 == j2){
+        //                         value = grid[i][j1] ;
+        //                     }
+        //                     else{
+        //                         value = grid[i][j2] + grid[i][j1]; 
+        //                     }
+        //                     if(j1 + dj1 >= 0 && j1 + dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m){
+        //                         value += dp[i+1][j1 + dj1][j2 + dj2]; 
+        //                     }
+        //                     else{
+        //                         value = Integer.MIN_VALUE;  
+        //                     }
+        //                     maxi = Math.max(maxi , value) ; 
+        //                 }
+        //             }
+        //             dp[i][j1][j2] = maxi ; 
+        //         }
+        //     }
+        // }
+        // return dp[0][0][m-1]; 
+
+        // space optimisation :- 
+
         int n = grid.length; 
         int m = grid[0].length; 
-        int dp[][][] = new int[n][m][m];
-        for(int[][] layer : dp){
-            for(int[] row : layer){
-                Arrays.fill(row , -1) ;
+        
+        int[][] next = new int[m][m];
+        int[][] curr = new int[m][m];
+
+        for(int j1 = 0; j1 < m; j1++) {
+            for(int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2) {
+                    next[j1][j2] = grid[n-1][j1]; 
+                } else {
+                    next[j1][j2] = grid[n-1][j1] + grid[n-1][j2]; 
+                }
             }
         }
-        return func(0 , 0 , m-1 , dp , grid , m , n); 
+
+        for(int i = n-2; i >= 0; i--) {
+            for(int j1 = 0; j1 < m; j1++) {
+                for(int j2 = 0; j2 < m; j2++) {
+                    int maxi = Integer.MIN_VALUE;
+                    for(int dj1 = -1; dj1 <= 1; dj1++) {
+                        for(int dj2 = -1; dj2 <= 1; dj2++) {
+                            int value = 0;
+
+                            if(j1 == j2) {
+                                value = grid[i][j1];
+                            } else {
+                                value = grid[i][j1] + grid[i][j2];
+                            }
+
+                            if(j1 + dj1 >= 0 && j1 + dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m) {
+                                value += next[j1 + dj1][j2 + dj2]; 
+                            }
+
+                            maxi = Math.max(maxi, value); 
+                        }
+                    }
+                    curr[j1][j2] = maxi;
+                }
+            }
+            
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+                    next[j1][j2] = curr[j1][j2]; 
+                }
+            }
+        }
+
+        return next[0][m-1]; 
     }
 }
